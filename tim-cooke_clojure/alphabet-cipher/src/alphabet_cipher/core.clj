64c91351cf60deb4fb2660d0ea7b8db17ed776d8
Tim Cooke <tim@trcooke.co.uk>
2016-10-11 12:41:51 +0100

    Removed duplication by extracting the variant function as a function argument.

--- tim-cooke_clojure/alphabet-cipher/src/alphabet_cipher/core.clj
+++ tim-cooke_clojure/alphabet-cipher/src/alphabet_cipher/core.clj
@@ -1,16 +1,15 @@
 (ns alphabet-cipher.core
   (:gen-class))
 
-(defn encode [msg key]
+(defn cipher [msg key direction]
   (->> msg
-        (map #(+ (- (int %2) (int \a)) (- (int %1) (int \a))) (flatten (cycle key)))
-        (map #(mod % 26))
-        (map #(char (+ % (int \a))))
-        (apply str)))
-
-(defn decode [msg key]
-  (->> msg
-      (map #(- (- (int %2) (int \a)) (- (int %1) (int \a))) (flatten (cycle key)))
+      (map #(direction (- (int %2) (int \a)) (- (int %1) (int \a))) (flatten (cycle key)))
       (map #(mod % 26))
       (map #(char (+ % (int \a))))
       (apply str)))
+
+(defn encode [msg key]
+  (cipher msg key +))
+
+(defn decode [msg key]
+  (cipher msg key -))
